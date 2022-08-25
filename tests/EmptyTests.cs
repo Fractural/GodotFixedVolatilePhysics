90af75ef378ead8df7d2fbce85211304e31c5162
Atlinx <atlinuxtm@gmail.com>
2022-08-25 00:30:02 -0400

    Add placeholder tests

---
+++ tests/EmptyTests.cs
@@ -0,0 +1,14 @@
+using System;
+using WAT;
+
+namespace Tests
+{
+    public class EmptyTests : WAT.Test
+    {
+        [Test]
+        public void EmptyTest()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
