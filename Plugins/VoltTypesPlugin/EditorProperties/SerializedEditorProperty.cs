bc368cc65987749e7517cbf423b1589fba3ce5b7
Atlinx <atlinuxtm@gmail.com>
2022-08-31 22:03:45 +0000

    Update release to output generated at bda0a99

--- Plugins/VoltTypesPlugin/EditorProperties/SerializedEditorProperty.cs
+++ Plugins/VoltTypesPlugin/EditorProperties/SerializedEditorProperty.cs
@@ -18,25 +18,18 @@ namespace Volatile.GodotEngine.Plugin
                 if (UseManualValue)
                     return ManualValue;
                 else
-                {
-                    GD.Print("getting actual " + GetEditedObject() + " is null? " + (GetEditedObject() == null));
                     return TypeSerializer.Deserialize(Data);
-                }
             }
             set
             {
                 if (UseManualValue)
                 {
                     ManualValue = value;
-                    GD.Print("emit changed with " + GetEditedObject() + " is null? " + (GetEditedObject() == null));
                     // We don't emit the value, in case it's not marshallable by Godot
                     EmitChanged(GetEditedProperty(), null);
                 }
                 else
-                {
-                    GD.Print("setting actual " + GetEditedObject() + " is null? " + (GetEditedObject() == null));
                     EmitChanged(GetEditedProperty(), TypeSerializer.Serialize(value));
-                }
             }
         }
         protected TTypeSerializer TypeSerializer { get; } = new TTypeSerializer();
