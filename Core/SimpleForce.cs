c0a640c428cc7ce1f10ee9b801ecda793ee593be
Atlinx <atlinuxtm@gmail.com>
2022-09-09 15:52:00 +0000

    Update release to output generated at da49681

--- Core/SimpleForce.cs
+++ Core/SimpleForce.cs
@@ -2,61 +2,61 @@
 
 namespace Volatile.GodotEngine
 {
-    [Tool]
-    public class SimpleForce : VoltNode2D
-    {
-        #region Force
-        private VoltVector2 force;
-        public VoltVector2 Force
-        {
-            get
-            {
+	[Tool]
+	public class SimpleForce : VoltNode2D
+	{
+		#region Force
+		private VoltVector2 force;
+		public VoltVector2 Force
+		{
+			get
+			{
 #if TOOLS
-                if (Engine.EditorHint)
-                    return VoltType.DeserializeOrDefault<VoltVector2>(_force);
-                else
+				if (Engine.EditorHint)
+					return VoltType.DeserializeOrDefault<VoltVector2>(_force);
+				else
 #endif
-                    return force;
-            }
-            set
-            {
+					return force;
+			}
+			set
+			{
 #if TOOLS
-                if (Engine.EditorHint)
-                    _force = VoltType.Serialize(value);
-                else
+				if (Engine.EditorHint)
+					_force = VoltType.Serialize(value);
+				else
 #endif
-                    force = value;
-            }
-        }
-        [Export(hintString: VoltPropertyHint.VoltVector2)]
-        private byte[] _force;
-        #endregion
+					force = value;
+			}
+		}
+		[Export(hintString: VoltPropertyHint.VoltVector2)]
+		private byte[] _force;
+		#endregion
 
-        private VolatileRigidBody body;
+		private IVolatileRigidBody body;
 
-        public override void _Ready()
-        {
-            base._Ready();
-            Force = VoltType.DeserializeOrDefault<VoltVector2>(_force);
+		public override void _Ready()
+		{
+			base._Ready();
+			Force = VoltType.DeserializeOrDefault<VoltVector2>(_force);
 
-            if (Engine.EditorHint)
-            {
-                SetPhysicsProcess(false);
-                return;
-            }
-            body = GetParent<VolatileRigidBody>();
-        }
+			if (Engine.EditorHint)
+			{
+				SetPhysicsProcess(false);
+				return;
+			}
+			body = GetParent<IVolatileRigidBody>();
+		}
 
-        public override string _GetConfigurationWarning()
-        {
-            if (!(GetParent() is VolatileRigidBody))
-                return $"{nameof(SimpleForce)} must be a child of a {nameof(VolatileRigidBody)}!";
-            return "";
-        }
+		public override string _GetConfigurationWarning()
+		{
+			if (!(GetParent() is IVolatileRigidBody))
+				return $"{nameof(SimpleForce)} must be a child of a {nameof(IVolatileRigidBody)}!";
+			return "";
+		}
 
-        public override void _PhysicsProcess(float delta)
-        {
-            if (body != null) body.AddForce(Force);
-        }
-    }
+		public override void _PhysicsProcess(float delta)
+		{
+			if (body != null) body.AddForce(Force);
+		}
+	}
 }
