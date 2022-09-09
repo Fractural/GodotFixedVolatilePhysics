727523a8a53148e9d4e613473019c25df4795212
Atlinx <atlinuxtm@gmail.com>
2022-09-09 15:45:34 +0000

    Update release to output generated at 059a0b2

--- Core/VolatileBodies/VolatileStaticBody.cs
+++ Core/VolatileBodies/VolatileStaticBody.cs
@@ -2,11 +2,13 @@
 
 namespace Volatile.GodotEngine
 {
-    [Tool]
-    public class VolatileStaticBody : VolatileBody
-    {
-        protected override VoltBody CreateBody(VoltWorld world, VoltShape[] shapes)
-            => world.CreateStaticBody(GlobalFixedPosition, GlobalFixedRotation, shapes, Layer, Mask);
+	public interface IVolatileStaticBody : IVolatileBody { }
 
-    }
+	[Tool]
+	public class VolatileStaticBody : VolatileBody, IVolatileStaticBody
+	{
+		protected override VoltBody CreateBody(VoltWorld world, VoltShape[] shapes)
+			=> world.CreateStaticBody(GlobalFixedPosition, GlobalFixedRotation, shapes, Layer, Mask);
+
+	}
 }
--- Core/VolatileShapes/VolatileShape.cs
+++ Core/VolatileShapes/VolatileShape.cs
@@ -1,159 +1,154 @@
 using Godot;
-using Volatile;
-using Godot.Collections;
-using Fractural;
 using FixMath.NET;
-using System;
 #if TOOLS
-using Volatile.GodotEngine.Plugin;
 #endif
 
 namespace Volatile.GodotEngine
 {
-    [Tool]
-    public abstract class VolatileShape : VoltNode2D
-    {
-        public abstract VoltShape PrepareShape(VoltWorld world);
-        public abstract Vector2 ComputeLocalCenterOfMass();
+	[Tool]
+	public abstract class VolatileShape : VoltNode2D
+	{
+		public abstract VoltShape PrepareShape(VoltWorld world);
+		public abstract Vector2 ComputeLocalCenterOfMass();
 
-        public override void _Ready()
-        {
-            base._Ready();
-            Density = VoltType.DeserializeOrDefault<Fix64>(_density);
-            Restitution = VoltType.DeserializeOrDefault<Fix64>(_restitution);
-            Friction = VoltType.DeserializeOrDefault<Fix64>(_friction);
-        }
+		public override void _Ready()
+		{
+			base._Ready();
+			Density = VoltType.DeserializeOrDefault<Fix64>(_density);
+			Restitution = VoltType.DeserializeOrDefault<Fix64>(_restitution);
+			Friction = VoltType.DeserializeOrDefault<Fix64>(_friction);
+		}
 
-        #region Density
-        protected Fix64 density;
-        public Fix64 Density
-        {
-            get
-            {
+		#region Density
+		protected Fix64 density;
+		public Fix64 Density
+		{
+			get
+			{
 #if TOOLS
-                if (Engine.EditorHint)
-                    return VoltType.DeserializeOrDefault<Fix64>(_density);
-                else
+				if (Engine.EditorHint)
+					return VoltType.DeserializeOrDefault<Fix64>(_density);
+				else
 #endif
-                    return density;
-            }
-            set
-            {
+					return density;
+			}
+			set
+			{
 #if TOOLS
-                if (Engine.EditorHint)
-                    _density = VoltType.Serialize(value);
-                else
+				if (Engine.EditorHint)
+					_density = VoltType.Serialize(value);
+				else
 #endif
-                    density = value;
-            }
-        }
-        [Export(hintString: VoltPropertyHint.Fix64)]
-        private byte[] _density = VoltType.Serialize(VoltConfig.DEFAULT_DENSITY);
-        #endregion
+					density = value;
+			}
+		}
+		[Export(hintString: VoltPropertyHint.Fix64)]
+		private byte[] _density = VoltType.Serialize(VoltConfig.DEFAULT_DENSITY);
+		#endregion
 
-        #region Restitution
-        protected Fix64 restitution;
-        public Fix64 Restitution
-        {
-            get
-            {
+		#region Restitution
+		protected Fix64 restitution;
+		public Fix64 Restitution
+		{
+			get
+			{
 #if TOOLS
-                if (Engine.EditorHint)
-                    return VoltType.DeserializeOrDefault<Fix64>(_restitution);
-                else
+				if (Engine.EditorHint)
+					return VoltType.DeserializeOrDefault<Fix64>(_restitution);
+				else
 #endif
-                    return restitution;
-            }
-            set
-            {
+					return restitution;
+			}
+			set
+			{
 #if TOOLS
-                if (Engine.EditorHint)
-                    _restitution = VoltType.Serialize(value);
-                else
+				if (Engine.EditorHint)
+					_restitution = VoltType.Serialize(value);
+				else
 #endif
-                    restitution = value;
-            }
-        }
-        [Export(hintString: VoltPropertyHint.Fix64)]
-        public byte[] _restitution = VoltType.Serialize(VoltConfig.DEFAULT_RESTITUTION);
-        #endregion
+					restitution = value;
+			}
+		}
+		[Export(hintString: VoltPropertyHint.Fix64)]
+		public byte[] _restitution = VoltType.Serialize(VoltConfig.DEFAULT_RESTITUTION);
+		#endregion
 
-        #region Friction
-        protected Fix64 friction;
-        public Fix64 Friction
-        {
-            get
-            {
+		#region Friction
+		protected Fix64 friction;
+		public Fix64 Friction
+		{
+			get
+			{
 #if TOOLS
-                if (Engine.EditorHint)
-                    return VoltType.DeserializeOrDefault<Fix64>(_friction);
-                else
+				if (Engine.EditorHint)
+					return VoltType.DeserializeOrDefault<Fix64>(_friction);
+				else
 #endif
-                    return friction;
-            }
-            set
-            {
+					return friction;
+			}
+			set
+			{
 #if TOOLS
-                if (Engine.EditorHint)
-                    _friction = VoltType.Serialize(value);
-                else
+				if (Engine.EditorHint)
+					_friction = VoltType.Serialize(value);
+				else
 #endif
-                    friction = value;
-            }
-        }
-        [Export(hintString: VoltPropertyHint.Fix64)]
-        public byte[] _friction = VoltType.Serialize(VoltConfig.DEFAULT_FRICTION);
-        #endregion
+					friction = value;
+			}
+		}
+		[Export(hintString: VoltPropertyHint.Fix64)]
+		public byte[] _friction = VoltType.Serialize(VoltConfig.DEFAULT_FRICTION);
+		#endregion
 
 #if TOOLS
-        [Signal]
-        public delegate void EditingChanged(bool editing);
+		[Signal]
+		public delegate void EditingChanged(bool editing);
 
-        private bool editing;
-        [Export]
-        public bool Editing
-        {
-            get => editing;
-            set
-            {
-                editing = value;
-                EmitSignal(nameof(EditingChanged), value);
-            }
-        }
+		private bool editing;
+		[Export]
+		public bool Editing
+		{
+			get => editing;
+			set
+			{
+				editing = value;
+				EmitSignal(nameof(EditingChanged), value);
+			}
+		}
 #endif
 
-        [Export]
-        public bool DebugDraw { get; set; } = false;
+		[Export]
+		public bool DebugDraw { get; set; } = false;
 
-        public Color GetShapeDrawColor()
-        {
-            Color color = Palette.DynamicBody;
-            var parent = GetParent();
-            if (parent != null)
-            {
-                if (parent is VolatileStaticBody)
-                    color = Palette.StaticBody;
-                else if (parent is VolatileKinematicBody)
-                    color = Palette.KinematicBody;
-                else if (parent is VolatileArea)
-                    color = Palette.AreaBody;
-            }
-            return color;
-        }
+		public Color GetShapeDrawColor()
+		{
+			Color color = Palette.DynamicBody;
+			var parent = GetParent();
+			if (parent != null)
+			{
+				if (parent is IVolatileStaticBody)
+					color = Palette.StaticBody;
+				else if (parent is IVolatileKinematicBody)
+					color = Palette.KinematicBody;
+				else if (parent is IVolatileArea)
+					color = Palette.AreaBody;
+			}
+			return color;
+		}
 
-        public override void _Draw()
-        {
-            base._Draw();
-            if (!DebugDraw && !Engine.EditorHint) return;
+		public override void _Draw()
+		{
+			base._Draw();
+			if (!DebugDraw && !Engine.EditorHint) return;
 
-            var color = Palette.Accent;
-            var localCenterOfMass = ComputeLocalCenterOfMass();
-            var innerRadius = 1;
-            var outerRadius = 1.1f;
-            DrawLine(localCenterOfMass - new Vector2(innerRadius, 0), localCenterOfMass + new Vector2(innerRadius, 0), color);
-            DrawLine(localCenterOfMass - new Vector2(0, innerRadius), localCenterOfMass + new Vector2(0, innerRadius), color);
-            DrawArc(localCenterOfMass, innerRadius, 0, 2 * Mathf.Pi, 60, color);
-            DrawArc(localCenterOfMass, outerRadius, 0, 2 * Mathf.Pi, 60, color);
-        }
-    }
+			var color = Palette.Accent;
+			var localCenterOfMass = ComputeLocalCenterOfMass();
+			var innerRadius = 1;
+			var outerRadius = 1.1f;
+			DrawLine(localCenterOfMass - new Vector2(innerRadius, 0), localCenterOfMass + new Vector2(innerRadius, 0), color);
+			DrawLine(localCenterOfMass - new Vector2(0, innerRadius), localCenterOfMass + new Vector2(0, innerRadius), color);
+			DrawArc(localCenterOfMass, innerRadius, 0, 2 * Mathf.Pi, 60, color);
+			DrawArc(localCenterOfMass, outerRadius, 0, 2 * Mathf.Pi, 60, color);
+		}
+	}
 }
